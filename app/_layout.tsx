a64b1db51d1dc8367d3b11a7014c5c5120ff063a
claudiaps <clp.sampedro@gmail.com>
2024-08-15 18:54:55 -0300

    clean setup to create a new project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,6 @@
-import Header from "@/components/Header";
-import { ScrollView, StyleSheet } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import { Stack } from "expo-router";
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#e3bde3",
-    padding: 50,
-  },
-});
 
 export default function RootLayout() {
   useEffect(() => {
@@ -18,8 +9,12 @@ export default function RootLayout() {
 
   return (
     <Stack>
-      <Stack.Screen name="homedd" />
-      <Stack.Screen name="test" />
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Previsão do tempo",
+        }}
+      />
     </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,19 @@
-import { Link } from "expo-router";
-import { View, Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function Home() {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+const index = () => {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ color: "#f2f" }}>Home Screen</Text>
-      <Link href={"/test/explore"}>Test</Link>
+    <View style={styles.container}>
+      <Text>Tela inicial</Text>
     </View>
   );
-}
+};
+
+export default index;
