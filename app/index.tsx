af8b6a08bad1109b9f0041a6649a85824e53ba51
claudiaps <clp.sampedro@gmail.com>
2024-08-15 21:40:43 -0300

    improove css

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 type Weather = {
   location: {
@@ -12,6 +13,7 @@ type Weather = {
     last_updated: string;
     temp_c: number;
     is_day: number;
+    uv: string;
     condition: {
       text: string;
       code: number;
@@ -21,32 +23,62 @@ type Weather = {
 
 const index = () => {
   const [weather, setWeather] = useState<Weather>();
+  const [loading, setLoading] = useState<boolean>();
+
   const requestWeather = useCallback(async () => {
     try {
+      setLoading(true);
       const response =
         await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=c5673414b031477a9df01840240806&q=London&days=5&lang=pt
 `);
       setWeather(response.data);
     } catch (error) {
+      console.error(error);
     } finally {
+      setLoading(false);
     }
   }, []);
+
   useEffect(() => {
     requestWeather();
   }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>{weather?.current?.temp_c}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      {loading ? <ActivityIndicator size="large" /> : <></>}
+      <View style={styles.currentTempContainer}>
+        <Text style={styles.currentTemp}>{weather?.current?.temp_c}º</Text>
+        <View style={{ gap: 8 }}>
+          <Text style={styles.condition}>
+            {weather?.current?.condition?.text}
+          </Text>
+          <Text style={styles.condition}>
+            Índice UV: {weather?.current?.uv}
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
-
 export default index;
-
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#02467a",
     flex: 1,
-    justifyContent: "center",
+    padding: 24,
+  },
+  condition: {
+    fontSize: 20,
+    color: "#ffffff",
+  },
+  currentTempContainer: {
+    flexDirection: "row",
     alignItems: "center",
+    gap: 16,
+  },
+  currentTemp: {
+    fontSize: 64,
+    color: "#ffffff",
+    fontWeight: "500",
   },
 });
